0b86be9c8f851a36129faaef48603c4c466d1c71
Haseeb Asif <haseeb1431@gmail.com>
2019-05-19 12:56:36 +0200

    initial database DML

---
+++ server/SQL/ioslbackendDB.sql
@@ -0,0 +1,63 @@
+CREATE DATABASE "ioslbackendDB"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1;
+
+
+
+go;
+
+CREATE TABLE public."Person"
+(
+    "ID" serial NOT NULL,
+    "FullName" character varying(100) NOT NULL,
+    "Email" character varying(100) NOT NULL,
+    "Password" character varying(250) NOT NULL,
+    "DateOfBirth" date,
+    "PersonType" integer DEFAULT 0,
+    "PersonRole" integer DEFAULT 0,
+    "PicturePath" character varying(250),
+    PRIMARY KEY ("ID")
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE public."Person"
+    OWNER to postgres;
+
+
+
+CREATE TABLE public."Address"
+(
+    "AddressID" serial NOT NULL,
+    "StreetAddress" character varying(150),
+    "City" character varying(150),
+    "Country" character varying(50),
+    "PostCode" integer,
+    PRIMARY KEY ("AddressID")
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE public."Address"
+    OWNER to postgres;    
+
+
+CREATE TABLE public."Orders"
+(
+    "OrderID" serial NOT NULL,
+    "PickAddressID" integer NOT NULL,
+    "DropAddressID" integer NOT NULL,
+    "PickDate" date,
+    "ArrivalDate" date,
+    PRIMARY KEY ("OrderID")
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE public."Orders"
+    OWNER to postgres;    
